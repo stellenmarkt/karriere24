15c5a054e2301eb7fcf722b7e6f25a476d2a918b
mott <webmaster@michael-ott.org>
2018-09-07 16:11:33 +0200

    customize xml export

--- config/module.config.php
+++ config/module.config.php
@@ -30,9 +30,9 @@ return array(
             'templates/default/index' => __DIR__ . '/../view/templates/default/index.phtml',
             'content/jobs-terms-and-conditions' => __DIR__ . '/../view/agb.phtml',
             'main-navigation'      => __DIR__ . '/../view/main-navigation.phtml',
+            'jobs/export/feed'  => __DIR__ . '/../view/jobs/feed.stellenmarkt.phtml',
             'jobs/jobboard/index.ajax.phtml' => __DIR__ . '/../view/jobs/index.ajax.phtml',
             'jobs/jobboard/index' => __DIR__ . '/../view/jobs/index.phtml',
-            'jobs/export/feed'  => __DIR__ . '/../view/jobs/feed.stellenmarkt.phtml',
         ),
     ),
     'translator'   => array(
