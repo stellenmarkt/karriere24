fd2416b4e24fdae4e1623cb647bfca1f9dfa51ea
Carsten Bleek <bleek@cross-solution.de>
2019-06-13 12:36:10 +0200

    initial deployment. #59

---
+++ deploy.php
@@ -0,0 +1,45 @@
+<?php
+namespace Deployer;
+
+require 'recipe/zend_framework.php';
+
+// Project name
+set('application', 'karriere24.de');
+
+// Project repository
+set('repository', 'git@gitlab.cross-solution.de:HPL/Karriere24.git');
+
+// Shared files/dirs between deploys 
+add('shared_files', [
+    'test/sandbox/public/.htaccess',
+    'test/sandbox/public/robots.txt',
+]);
+
+add('shared_dirs', [
+    'test/sandbox/var/log',
+    'test/sandbox/var/cache',
+    'test/sandbox/config/autoload',
+    'test/sandbox/public/static',	// used by eg. organization logos
+]);
+
+// Writable dirs by web server 
+add('writable_dirs', [
+    'test/sandbox/var/cache',
+    'test/sandbox/var/log',
+    'test/sandbox/public/static',
+]);
+
+set('default_stage', 'prod');
+
+// Hosts
+
+host('upcoming.karriere24.de')
+    ->user('yawik')
+    ->stage('prod')
+    ->multiplexing(false) 
+    ->set('deploy_path', '/var/www/production')
+    ->set('writableusesudo', true);   
+    
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
